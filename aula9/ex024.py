17eda224f4153a263b74b34dc86dccb8054d922d
Wal221 <walmir.cardoso@estudante.ifgoiano.edu.br>
2023-05-03 15:58:43 -0300

    Exercicios com Strings

---
+++ aula9/ex024.py
@@ -0,0 +1,4 @@
+city = str(input('Em que cidade voce nasceu? ')).strip()
+print(city[:5].upper() == 'SANTO')
+
+
---
+++ aula9/ex025.py
@@ -0,0 +1,3 @@
+nome = str(input('Qual o seu nome? '))
+dividido = nome.split()
+print(dividido[0] )
